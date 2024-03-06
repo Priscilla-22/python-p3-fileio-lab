0d5288f1fe4158a6469f361e33e66c54872e1455
Priscilla-22 <priscillamw123@gmail.com>
2024-03-06 06:04:24 +0300

    feat: file I/O

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
-    pass
+    file_name_extension = f'{file_name}.txt'
+    with open(file_name_extension, mode='w', encoding='utf-8') as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    file_name_extension = f'{file_name}.txt'
+
+    with open(file_name_extension, mode='a', encoding='utf-8') as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    file_name_extension = f'{file_name}.txt'
+
+    with open(file_name_extension, encoding='utf-8') as file:
+        return file.read()
